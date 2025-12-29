458e105c53dd54a5de3ee547bdaa37758b72209a
NeeradaSugatri <mlvsneeradasugatri@gmail.com>
2025-12-29 18:47:15 +0530

    Add files via upload

---
+++ week1/L1_1.py
@@ -0,0 +1,8 @@
+name = "Neerada Suagtri"
+age = 17
+height = 165
+student = True
+print("My Name is: ",name)
+print("My age is: ",age)
+print("My height is: ",height)
+print("I am a student: ",student)
