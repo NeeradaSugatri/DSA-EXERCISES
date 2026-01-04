66bf991b2fcfb3d704e0d4386c496d2c2bdede14
NeeradaSugatri <mlvsneeradasugatri@gmail.com>
2026-01-04 16:13:20 +0530

    Add files via upload

---
+++ week1/q8.py
@@ -0,0 +1,9 @@
+light = input("Enter light color: ")
+if(light == "red"):
+    print("STOP")
+elif(light == "yellow"):
+    print("PREPARE TO GO")
+elif(light == "green"):
+    print("YOU CAN GO")
+else:
+    print("WRONG INPUT")
