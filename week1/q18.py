059d5a08db287bad78c64c078be06a26b4567b0c
NeeradaSugatri <mlvsneeradasugatri@gmail.com>
2026-01-04 22:17:40 +0530

    Add files via upload

---
+++ week1/q18.py
@@ -0,0 +1,11 @@
+secret = 7
+guess = int(input("Guess the number: "))
+
+while guess != secret:
+    if guess > secret:
+        print("Too high")
+    else:
+        print("Too low")
+    guess = int(input("Guess again: "))
+
+print("correct guess!")
